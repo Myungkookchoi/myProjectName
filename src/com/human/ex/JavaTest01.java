868fdb54ebfd19b995d02df24ec957ff4eac4f4b
audrn <audrn@DESKTOP-S1VII4A>
2022-03-03 14:26:17 +0900

    first commit

---
+++ src/com/human/ex/JavaTest01.java
@@ -0,0 +1,33 @@
+package com.human.ex;
+
+public class JavaTest01 {
+	/*
+	 * 프로그램의 시작.
+	 * 단축키 설정
+	 */
+		public static void main(String[] args) {
+		
+		
+		//ctrl + 방향키	 단어기준이동
+		//shift + 방향키		영역선택
+		//shift + ctrl + 방향키 단어기준이동하면서 영역선택
+		//ctrl + / 주석 설정 및 해제
+		//home, end 현 줄에서 앞부분, 뒷부분 이동
+		//ctrl + shift + +,- 글자 크기 조절
+		//ctrl + f 모두 바꾸기
+		//ctrl + z 이전으로
+		//ctrl + y 앞전으로
+		//ctrl + c, v, x 복사, 붙여넣기, 잘라내기
+		
+			System.out.println("hi");
+			System.out.println("hi");
+			System.out.println("hi");
+			System.out.println("hi");
+			System.out.println("hi");
+			System.out.println("hi");
+			System.out.println("hi");
+			System.out.println("hi");
+		}
+	
+	}
+
---
+++ src/com/human/ex/JavaTest02.java
@@ -0,0 +1,55 @@
+package com.human.ex; 
+
+public class JavaTest02 {			//키워드
+	public static void main(String[] args) {	//식별자
+		System.out.println("Hello World~"); 
+		
+		
+		System.out.println("안녕자바"); //문제 3
+		
+		System.out.println("***************************"); //문제 4 
+		System.out.println("* 최 명국   01000000000     *");
+		System.out.println("* audrnr3577@gmail.com    *");
+		System.out.println("***************************");
+		System.out.println("");
+		System.out.println("*****"); //문제 5-1
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println("");
+		System.out.println("*");	//문제 5-2
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println("*****");
+		System.out.println("");
+		System.out.println("    *");	//문제 5-3
+		System.out.println("   **");
+		System.out.println("  ***");
+		System.out.println(" ****");
+		System.out.println("*****");
+		System.out.println("");
+		System.out.println("******************");	//문제 6
+		System.out.println("*    성적표       *");
+		System.out.println("******************");
+		System.out.println("* 국어   50       *");
+		System.out.println("******************");
+		System.out.println("* 영어   50       *");
+		System.out.println("******************");
+		System.out.println("* 수학   50       *");
+		System.out.println("******************");
+		System.out.println("* 컴퓨터 학과 홍길동 *");
+		System.out.println("******************");
+		System.out.println("");
+		System.out.println("일\t월\t화\t수\t목\t금\t토");	//문제 7
+		
+		System.out.println("1\t2\t3\t4\t5\t6\t7");
+		System.out.println("8\t9\t10\t11\t12\t13\t14");
+		System.out.println("15\t16\t17\t18\t19\t20\t21");
+		System.out.println("22\t23\t24\t25\t26\t27\t28");
+		System.out.println("29\t30\t31\t\t\t\t");
+		
+	}
+}
+

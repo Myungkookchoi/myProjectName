54126a1c77e5c0170c0e7f22a38f13470ed18a3d
audrn <audrn@DESKTOP-S1VII4A>
2022-03-03 21:55:40 +0900

    tset

---
+++ src/com/human/ex/TSET.java
@@ -0,0 +1,13 @@
+package com.human.ex;
+
+public class TSET {
+
+	public static void main(String[] args) {
+		for (int i = 10; i >= 0; i--) {
+			System.out.println(i);
+			if (i <= 10) {
+				System.out.println("발사");
+			}
+		}
+	}
+}
